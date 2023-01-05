11b5901be0ba9a9a920524ced27595a98179f043
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2023-01-05 11:28:44 -0500

    fixes the controller test

--- dotacounterpicker/src/test/kotlin/bista/shiddarth/dotacounterpicker/controller/CounterControllerTest.kt
+++ dotacounterpicker/src/test/kotlin/bista/shiddarth/dotacounterpicker/controller/CounterControllerTest.kt
@@ -4,7 +4,6 @@ import bista.shiddarth.dotacounterpicker.service.CounterService
 import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import reactor.core.publisher.Mono
@@ -17,18 +16,13 @@ internal class CounterControllerTest {
     @Nested
     inner class GetCounters {
 
-        @Disabled
         @Test
         fun `5 counters are returned`() {
-            every { counterService.getTopFiveCounters("Axe") } returns
-                    Mono.just(listOf("Beast", "Drow", "Bat Rider", "Tusk", "Bane"))
+            val expected = Mono.just(listOf("Beast", "Drow", "Bat Rider", "Tusk", "Bane"))
+            every { counterService.getTopFiveCounters(any()) } returns expected
 
             val result = testController.getCounters("Axe")
-            assertThat(result).isEqualTo(
-                listOf(
-                    "Beast", "Drow", "Bat Rider", "Tusk", "Bane"
-                )
-            )
+            assertThat(result).isEqualTo(expected)
         }
     }
 
